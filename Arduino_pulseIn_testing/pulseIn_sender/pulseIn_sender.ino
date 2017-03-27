26ee10520dc7c8fc76064fe8d0a8a07d6e23cb92
Robert Zacharias <bobby.zacharias@gmail.com>
2017-03-26 21:06:33 -0400

    added sendPattern test sequence

--- Arduino_pulseIn_testing/pulseIn_sender/pulseIn_sender.ino
+++ Arduino_pulseIn_testing/pulseIn_sender/pulseIn_sender.ino
@@ -25,68 +25,9 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(12) == LOW) {
-    if (directPortAccess) PORTB = B1; // set 8 HIGH
-    else digitalWrite(8, HIGH);
-    delayMicroseconds(1000);
-    if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
-    else digitalWrite(8, LOW);
-    delay(50);
-  }
+  if (digitalRead(12) == LOW) sendPulse(1000);
 }
 
-void sendPattern() {
-
-  // test sending 100 100's
-  for (int i = 0; i < 100; i++){
-    if (directPortAccess) PORTB = B1; // set 8 HIGH
-    else digitalWrite(8, HIGH);
-    delayMicroseconds(100);
-    if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
-    else digitalWrite(8, LOW);
-    delay(2);
-  }
-
-  // test sending 100 500's
-  for (int i = 0; i < 100; i++){
-    if (directPortAccess) PORTB = B1; // set 8 HIGH
-    else digitalWrite(8, HIGH);
-    delayMicroseconds(500);
-    if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
-    else digitalWrite(8, LOW);
-    delay(2);
-  }
-
-  // test sending 100 1000's
-  for (int i = 0; i < 100; i++){
-    if (directPortAccess) PORTB = B1; // set 8 HIGH
-    else digitalWrite(8, HIGH);
-    delayMicroseconds(1000);
-    if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
-    else digitalWrite(8, LOW);
-    delay(2);
-  }
-  
-  // test incrementing by single microseconds
-  for (int i = 10; i < 1000; i++) {
-    if (directPortAccess) PORTB = B1; // set 8 HIGH
-    else digitalWrite(8, HIGH);
-    delayMicroseconds(i);
-    if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
-    else digitalWrite(8, LOW);
-    delay(2);
-  }
-
-  // test incrementing by 5 microseconds
-  for (int i = 10; i < 5000; i = i + 5) {
-    if (directPortAccess) PORTB = B1; // set 8 HIGH
-    else digitalWrite(8, HIGH);
-    delayMicroseconds(i);
-    if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
-    else digitalWrite(8, LOW);
-    delay(10);
-  }
-}
 
 void sendPulse(int width){
    if (directPortAccess) PORTB = B1; // set 8 HIGH
@@ -94,6 +35,35 @@ void sendPulse(int width){
     delayMicroseconds(width);
     if (directPortAccess) PORTB = B10000; // set 8 LOW and return 12 to HIGH
     else digitalWrite(8, LOW);
-    delayMicroseconds(width * 10);
+    delayMicroseconds(width * 10); // long enough pause between pulses
 }
+
+void sendPattern() {
+
+  // test sending 100 100's
+  for (int i = 0; i < 100; i++){
+    sendPulse(100);
+  }
+
+  // test sending 100 500's
+  for (int i = 0; i < 100; i++){
+    sendPulse(500);
+  }
+
+  // test sending 100 1000's
+  for (int i = 0; i < 100; i++){
+    sendPulse(1000);
+  }
+  
+  // test incrementing by single microseconds
+  for (int i = 10; i < 1000; i++) {
+    sendPulse(i);
+  }
+
+  // test incrementing by 5 microseconds
+  for (int i = 10; i < 5000; i = i + 5) {
+    sendPulse(i)
+  }
+}
+
 
