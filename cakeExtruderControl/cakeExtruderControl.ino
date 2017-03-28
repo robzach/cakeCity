c9fb6d1609050b2685223badc016ac3ef5d2220b
Robert Zacharias <bobby.zacharias@gmail.com>
2017-03-27 22:15:33 -0400

    motor takes only direction and on/off control; added jog buttons

--- cakeExtruderControl/cakeExtruderControl.ino
+++ cakeExtruderControl/cakeExtruderControl.ino
@@ -2,14 +2,9 @@
    Simple motor control based on incoming pulse width signal
 
    Motor has dedicated H-bridge driven by pins 5 and 6,
-   and receives a 5V signal on pin 8. A position-driven
-   potentiometer on the motor is read on A0.
-
-   Control system should send pulses using transmitting a value of mx+b like so:
-      m = gap between pulses, PULSESTEP below
-      b = minimum pulse width, MINPULSE below
-      x = actual datum, should range from 0 to 1023
-      at m = 5 and b = 10, min pulse sent should be 10us and max should be 5125us
+   and receives a 5V control signal on pin 8. The input signal
+   is either extend, retract, or stop, and the motor will keep
+   going until it gets a different instruction.
 
    3-24-17 initial version
    3-26-17
@@ -18,89 +13,118 @@
        added nonblocking pulse receive indicator light
        added proportional motor control
        broke LED functions out
+   3-27-17
+       rewrote to accept extend, retract, or stop commands rather than position
+       added jog pushbuttons
+       added stop button, read by interrupt process for greater immediacy
+       TO POSSIBLY DO:
+           pot feedback governs motor speed through feedback
+           (but jog buttons run at non-feedback speed)
+           multiple extend-speed commands to be read (fast, med, slow?)
 
    Robert Zacharias
    released to the public domain by the author
 */
 
+// timing variables
+const int STOPPULSE = 2000; // two millisecond (2000 uS) pulse is stop command
+const int EXTENDPULSE = 4000; // four millisecond (4000 uS) pulse is extend command
+const int RETRACTPULSE = 6000; // six millisecond (6000 uS) pulse is retract command
+const int PULSEFUZZ = 200; // microseconds of permissible slop in pulse signal
+const int INDICATORWAIT = 500; // number of milliseconds LED should stay lit after pulse received
 
-const int MINPULSE = 10; // shortest received pulse in microseconds
-const int PULSESTEP = 5; // microseconds of gap between expected pulse widths
+// output pins
 const int MOTORCONTROL1 = 5; // if driven positive, will extend linear actuator
 const int MOTORCONTROL2 = 6; // if driven positive, will retract linear actuator
 const int EXTENDINGLED = 9;
 const int RETRACTINGLED = 10;
 const int ATPOSLED = 13;
 const int PULSERECEIVELED = 12;
-const int INDICATORWAIT = 500; // number of milliseconds LED should stay lit after pulse received
-const int MOTORPOSITION = A0;
+const int ESTOPLED = 8;
+
+// input pins
 const int SIGNALPIN = 2; // on an Uno, pins 2 and 3 can have interrupts
-const int PWMMIN = 40; // smallest PWM signal to transmit; greater than 0 to eliminate deadband
-const int PWMMAX = 255; // largest PWM signal to transmit
+const int EXTENDBUTTON = 4; // pushbutton to jog
+const int RETRACTBUTTON = 7; // pushbutton to jog
+const int ESTOPBUTTON = 3; // the other interrupt;
 
 long timer;
-
+char mode = 's';
 int rawPosCommand;
-int posCommand;
 
 void setup() {
 
-  attachInterrupt(digitalPinToInterrupt(SIGNALPIN), readPulse, RISING);
-
+  // output pins
   pinMode(MOTORCONTROL1, OUTPUT);
   pinMode(MOTORCONTROL2, OUTPUT);
   pinMode(EXTENDINGLED, OUTPUT);
   pinMode(RETRACTINGLED, OUTPUT);
   pinMode(ATPOSLED, OUTPUT);
   pinMode(PULSERECEIVELED, OUTPUT);
+  pinMode(ESTOPLED, OUTPUT);
 
-  pinMode(MOTORPOSITION, INPUT);
+  // input pins
+  pinMode(SIGNALPIN, INPUT);
+  pinMode(EXTENDBUTTON, INPUT_PULLUP);
+  pinMode(RETRACTBUTTON, INPUT_PULLUP);
+  pinMode(ESTOPBUTTON, INPUT_PULLUP);
 
   digitalWrite(MOTORCONTROL1, LOW);
   digitalWrite(MOTORCONTROL2, LOW);
   statusLEDs('x'); // turn off indicator LEDs to start
+
+  attachInterrupt(digitalPinToInterrupt(SIGNALPIN), readPulse, RISING);
+  attachInterrupt(digitalPinToInterrupt(ESTOPBUTTON), estop, LOW);
 }
 
 void loop() {
-  driveMotor(posCommand);
+  driveMotor(mode);
+  if (digitalRead(EXTENDBUTTON == LOW) mode = 'e'; // jog extend
+  if (digitalRead(RETRACTBUTTON == LOW) mode = 'r'; // jog retract
   digitalWrite(PULSERECEIVELED, ((millis() - timer) < INDICATORWAIT));
 }
 
-// the interrupt service routine
+// the interrupt service routine to detect stop button
+void estop() {
+  // shut off motor and blink eStop light forever (blocking anything else until restart)
+  digitalWrite(MOTORCONTROL1, LOW);
+  digitalWrite(MOTORCONTROL2, LOW);
+  statusLEDs('x');
+  while (true) {
+    digitalWrite(ESTOPLED, HIGH);
+    delay(200);
+    digitalWrite(ESTOPLED, LOW);
+    delay(200);
+  }
+}
+
+// the interrupt service routine to detect incoming pulse
 void readPulse() {
   rawPosCommand = pulseIn(SIGNALPIN, HIGH);
-  posCommand = int((rawPosCommand - MINPULSE) / PULSESTEP);
+  if (abs(rawPosCommand - STOPPULSE) < PULSEFUZZ) mode = 's';
+  else if (abs(rawPosCommand - EXTENDPULSE) < PULSEFUZZ) mode = 'e';
+  else if (abs(rawPosCommand - RETRACTPULSE) < PULSEFUZZ) mode = 'r';
   timer = millis();
 }
 
 void driveMotor(int command) {
-  int pos = analogRead(MOTORPOSITION);
-
-  // need to extend
-  if (pos < command) {
-    analogWrite(MOTORCONTROL1, driveSpeed(pos, command));
-    digitalWrite(MOTORCONTROL2, LOW);
-    statusLEDs('e');
+  switch (command) {
+    case 'e':
+      digitalWrite(MOTORCONTROL1, HIGH);
+      digitalWrite(MOTORCONTROL2, LOW);
+      statusLEDs('e');
+      break;
+    case 'r':
+      digitalWrite(MOTORCONTROL1, LOW);
+      digitalWrite(MOTORCONTROL2, HIGH);
+      statusLEDs('e');
+      break;
+    case 's':
+      digitalWrite(MOTORCONTROL1, LOW);
+      digitalWrite(MOTORCONTROL2, LOW);
+      statusLEDs('k');
+      break;
   }
-
-  // need to retract
-  else if (pos > command) {
-    digitalWrite(MOTORCONTROL1, LOW);
-    analogWrite(MOTORCONTROL2, driveSpeed(pos, command));
-    statusLEDs('r');
-  }
-
-  // at target
-  else {
-    digitalWrite(MOTORCONTROL1, LOW);
-    digitalWrite(MOTORCONTROL2, LOW);
-    statusLEDs('k');
-  }
-}
-
-int driveSpeed(int inPos, int inCommand) {
-  int outspeed = map(abs(inPos - inCommand), 0, 1023, PWMMIN, PWMMAX);
-  return outspeed;
 }
 
 void statusLEDs(char in) {
@@ -124,7 +148,7 @@ void statusLEDs(char in) {
     default:
       digitalWrite(EXTENDINGLED, LOW);
       digitalWrite(RETRACTINGLED, LOW);
-      digitalWrite(ATPOSLED, HIGH);
+      digitalWrite(ATPOSLED, LOW);
       break;
   }
 }
