a5b45c6f43ccb4fe2912ee1429ed951ee99e2bc2
Robert Zacharias <bobby.zacharias@gmail.com>
2017-03-31 18:44:19 -0400

    rewrote pulseIn to use interrupt and port access; added results of old pulseIn testing; updated schematic

--- Arduino_pulseIn_testing/pulseIn_receiver/pulseIn_receiver.ino
+++ Arduino_pulseIn_testing/pulseIn_receiver/pulseIn_receiver.ino
@@ -1,24 +1,66 @@
 /*
- * for use with pulseIn_sender sketch, to test the fidelity and
- * accuracy of the Arduino's pulse width reading.
- * 
- * I used two knockoff Nanos for testing.
- * 
- * testing on 3/24/17 demonstrated that most reads came through
- * clear, with about 4 microseconds of variation (2 in each 
- * direction) from the modal value being transmitted. I have not
- * run actual statistics on the received values.
- * 
- * receives pulses on pin 8. Be sure the Arduinos' grounds
- * are tied together for good data transmission.
- */
+   Read input data from digital output line in ABB robot
+
+   3/30/17 revising to explicitly use interrupts since some of the data
+   coming through testing with Josh Bard on the robot was questionable.
+   (The testing was using a prior version that simply read pulseIn and printed
+   the result to the serial terminal; that version's source as well as a long
+   stream of test results is available in pulseIn_receiver-testing-3-31-17.txt)
+
+   Robert Zacharias, rz@rzach.me
+   released by the author to the public domain
+*/
+
+const int READPIN = 2; // do not change this casually; it needs to be an interrupt pin and also the PIND port access bitmask below assumes this will be pin 2
+long startTime, stopTime, diff;
+int fuzz = 500; // number of microseconds to fudge on either side of pulse width result
+bool lastState = 0; // state of the pin last time the interrupt was triggered
+
+// target pulse width in microseconds of to-be-defined commands. Array length can be changed,
+// but check that motorMode reasonably matches
+int command[]= {2000, 4000, 6000, 8000};
 
 void setup() {
-  pinMode(8, INPUT);
+  pinMode(READPIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(READPIN), readPulse, CHANGE);
   Serial.begin(115200);
 }
 
 void loop() {
-  int in = pulseIn(8, HIGH);
-  if (in) Serial.println(in);
+  // drive motor based on pulse read
+  for (int i = 0; i < sizeof(command); i++){
+    if ( abs(diff-command[i]) < fuzz ) motorMode(i);
+  }
+  Serial.println(i);
 }
+
+void readPulse() {
+//  int readState = digitalRead(READPIN); // digitalRead is slower
+  int readState = (PIND & (0b00000100)); // port access is quicker
+  if (readState == lastState) return; // error state: two rises or falls in a row
+  if (readState) startTime = micros(); // if just went high, start timer
+  else stopTime = micros(); // if just went low, stop timer
+  diff = stopTime - startTime;
+  lastState = readState;
+}
+
+void motorMode(int in){
+  switch (in) {
+    case 0:
+      // drive motor in some way
+      break;
+    case 1:
+      // drive motor in some way
+      break;
+    case 2:
+      // drive motor in some way
+      break;
+    case 3:
+      // drive motor in some way
+      break;
+    default:
+      // stop motor 
+      break;    
+  }
+}
+
